bca4af05027e6b99f3861624b8268101dd19a75d
pj perry <pjperry28@gmail.com>
2022-06-01 22:08:13 -0700

    add discription function to all flash cards , final commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,30 @@ $(document).ready(function() {
     $("#operators").toggle();
     $("#oper").toggle();
   });
+  $("#variables,#varib").click(function() {
+    $("#variables").toggle();
+    $("#varib").toggle();
+  });
+  $("#funkee,#funk").click(function() {
+    $("#funkee").toggle();
+    $("#funk").toggle();
+  });
+  $("#methods,#method1").click(function() {
+    $("#methods").toggle();
+    $("#method1").toggle();
+  });
+  $("#arguments,#args1").click(function() {
+    $("#arguments").toggle();
+    $("#args1").toggle();
+  });
+  $("#strings,#strings1").click(function() {
+    $("#strings").toggle();
+    $("#strings1").toggle();
+  });
+  $("#booleans,#bool").click(function() {
+    $("#booleans").toggle();
+    $("#bool").toggle();
+  });  
 });
 
 
