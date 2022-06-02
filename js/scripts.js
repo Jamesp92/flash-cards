ed5cfa30ecef19f7d8bd6072d1120daf9b76a721
pj perry <pjperry28@gmail.com>
2022-06-01 21:06:16 -0700

    clicking on java and operator title will open discription and clicking on discription will open back to title

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#javascript , #java").click(function() {
+    $("#javascript").toggle();
+    $("#java").toggle();
+  });
+
+  $("#operators,#oper").click(function() {
+    $("#operators").toggle();
+    $("#oper").toggle();
+  });
+});
+
+
+
+
+
+
